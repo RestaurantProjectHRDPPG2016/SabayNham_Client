927ee0b8812be525487dd1fc497e12b6b5ae7120
USER <USER@USER-PC>
2016-08-01 22:41:01 +0700

    mytestupdate

--- src/main/java/res/java/client/pp/g2/controller/MainController.java
+++ src/main/java/res/java/client/pp/g2/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	@RequestMapping(value="/")
 	public String home(){
+		System.out.println(" Hello World !!!");
 		return "front/layout/home_view";
 	}
 	@RequestMapping(value="/about")
